3febf8ae156c091d7b970021a58dcebb7fce323f
Aeter4nam <aeter4nam@gmail.com>
2017-03-21 22:23:05 +0600

    Стек

--- Main.java
+++ Main.java
@@ -1,13 +1,33 @@
 class Stack {
     
+    int intArray[] = new int[8];
+    int index = -1;
     
+    void push(int intValue) {
+        if(index <= 7) {
+            System.out.println("Array full");
+        }
+        else {
+            intArray[++index] = intValue;
+        }
+    }
+    
+    int pull() {
+        if(index < 0) {
+            System.out.println("Array empty");
+            return 0;
+        }
+        else {
+            return intArray[index--];
+        }
+    }
     
 }
 
 class Main {
     public static void main(String argc[]) {
         
-        System.out.println("Hello World");
+        
         
     }
 }
